f0bf10f4a4537f967d8c2ca029e921b55b0d7567
Ludovic Demol <demol.ludovic@pasdecalais.fr>
2024-10-10 08:12:57 +0200

    feat: responsive

--- src/app/medicine/page.tsx
+++ src/app/medicine/page.tsx
@@ -16,7 +16,7 @@ export default function Component() {
 
     return (
         <>
-        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1 hidden md:block" aria-hidden="true">
         <svg width="1260" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
           <defs>
             <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
@@ -31,15 +31,14 @@ export default function Component() {
           </g>
         </svg>
       </div>
-
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Medico<span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Compare</span></h1>
+          <div className="text-center pb-12 md:pb-16 ">
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Medico<span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 ">Compare</span></h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Votre référence pour tout savoir a propos de vos médicaments.</p>
             </div>
-            <div className=" sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+            <div className="sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <MedicineAutocomplete />
               </div>
           </div>
